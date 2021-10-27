51c7da09d734317b3d0125401152784edc7c88b0
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-10-26 22:43:39 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,5 +1,29 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
 public class Controller {
 
+	@FXML
+	private ToggleGroup action;
+	
+	@FXML
+	private ToggleGroup status;
+	
+	@FXML
+	private TextField name;
+	
+	@FXML
+	private ToggleGroup major;
+	
+	@FXML
+	private TextField noCredits;
+	
+	@FXML
+	void add(ActionEvent event) {
+		
+	}
 }
