5c9bfb706bf1a7cdaf9ec181f11b6f190a7762d6
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-10-28 15:18:01 -0400

    Create Major.java

---
+++ Major.java
@@ -0,0 +1,9 @@
+package application;
+
+/**
+This class defines the object Major and lists all possible majors
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
+public enum Major {
+	CS, IT, BA, EE, ME
+}
